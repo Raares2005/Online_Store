6ba165e32749aa5b8e80b273e00f63562c67a978
Raares2005 <rares.croitoru8@gmail.com>
2021-10-09 23:24:03 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/** First Slider */
+$(".slider-one")
+.not(".slick-initialized")
+.slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    prevArrow: ".site-slider .slider-btn .prev",
+    nextArrow: ".site-slider .slider-btn .next",
+});
+
+
+/** Second Slider */
+$(".slider-two")
+.not(".slick-initialized")
+.slick({
+    prevArrow: ".site-slider-two .prev",
+    nextArrow: ".site-slider-two .next",
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplaySpeed: 3000,
+});
